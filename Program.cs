a3ba1e9fed6e444b6fc7fabb76113c2628a14c92
bakhtishod <bakhtishodstudent@gmail.com>
2024-09-30 16:23:26 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Shopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dataLength = 0;
+            do
+            {
+                dataLength = Int32.Parse(Console.ReadLine());
+            } while (!(dataLength >= 1 && dataLength <= 100));
+
+            string[] brandName = new string[dataLength];
+            int[] brandAmount = new int[dataLength];
+
+            for (int i = 0; i < dataLength; i++)
+            {
+                string nameInput = Console.ReadLine();
+                brandName[i] = nameInput;
+                int amountInput = Convert.ToInt32(Console.ReadLine());
+                brandAmount[i] = amountInput;
+            }
+
+            string firstBrand = brandName[0];
+            int totalAmount = 0;
+
+            for (int i = 0; i < dataLength; i++)
+            {
+                if (firstBrand == brandName[i])
+                {
+                    totalAmount += brandAmount[i];
+                }
+            }
+
+            Console.WriteLine(totalAmount);
+        }
+    }
+}
